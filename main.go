85b27c06b6900689dacd139b9f9a63fd76ab45f3
Dwcks Dinh <corclan19@gmail.com>
2023-07-15 04:25:18 -0700

    fix live restart using nodemon and add faq + contact templates

--- main.go
+++ main.go
@@ -9,65 +9,50 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func parseAndExecuteTpl(w http.ResponseWriter, page string, data any) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	// tplPath := filepath.Join("templates","home.gohtml")
-	tpl, err := template.ParseFiles("templates/home.gohtml")
+	tpl, err := template.ParseFiles(fmt.Sprintf("templates/%s.gohtml", page))
+
 	if err != nil {
 		log.Printf("parsing templates %v", err)
-		http.Error(w,"There was an error parsing the template",http.StatusInternalServerError)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
+
+	err = tpl.Execute(w, data)
 	if err != nil {
 		log.Printf("execute templates %v", err)
-		http.Error(w,"There was an error execute the template",http.StatusInternalServerError)
+		http.Error(w, "there was an error execute the template", http.StatusInternalServerError)
 		return
 	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// tplPath := filepath.Join("templates","home.gohtml")
+	parseAndExecuteTpl(w, "home", nil)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>this is contact page</h1>")
-	fmt.Fprint(w, id)
+	parseAndExecuteTpl(w, "contact", struct{ ID string }{ID: id})
 }
 
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w,r)
-// 	case "/contact":
-// 		contactHandler(w,r)
-// 	default:
-// 		http.Error(w,"page not found",http.StatusNotFound)
-// 	}
-// }
-
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w,r)
-// 	case "/contact":
-// 		contactHandler(w,r)
-// 	default:
-// 		http.Error(w,"page not found",http.StatusNotFound)
-// 	}
-// }
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	parseAndExecuteTpl(w, "faq", nil)
+}
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/",homeHandler)
-	r.Get("/contact",contactHandler)
-	r.Get("/contact/{id}",contactHandler)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request){
-		http.Error(w, "Page not found",http.StatusNotFound)
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/contact/{id}", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
 
 	fmt.Println("server start at port 3000")
 
-	http.ListenAndServe(":3000",r)
+	http.ListenAndServe(":3000", r)
 }
