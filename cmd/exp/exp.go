f8e659593af94d9b5b62ea9acad22c4b01d0c956
Dwcks Dinh <corclan19@gmail.com>
2023-07-15 23:37:53 +0700

    example of template

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,20 +3,29 @@ package main
 import (
 	"html/template"
 	"os"
+	"path/filepath"
 )
 
-type User struct{
-	Name string
-	Age int
-	Meta UserMeta
+type User struct {
+	Name  string
+	Age   int
+	Meta  UserMeta
+	Chats []string
+	Fl    float64
+	Map   map[string]string
+	Haha  bool
 }
 
-type UserMeta struct{
-	Visits int
+type UserMeta struct {
+	Visits struct {
+		Name string
+		Age  int
+	}
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	path := filepath.Join("cmd", "exp", "hello.gohtml")
+	t, err := template.ParseFiles(path)
 	if err != nil {
 		panic(err)
 	}
@@ -29,10 +38,16 @@ func main() {
 
 	user := User{
 		Name: "Dinh Dat Dinh Diem",
-		Age: 19,
-		Meta:  UserMeta{
-			Visits: 11,
+		Age:  19,
+		Meta: UserMeta{
+			Visits: struct {
+				Name string
+				Age  int
+			}{Name: "Dinh", Age: 20},
 		},
+		Chats: []string{"hello", "world"},
+		Fl:    3.4,
+		Map:   map[string]string{"hana": "koisi", "lee": "sin"},
 	}
 
 	err = t.Execute(os.Stdout, user)
